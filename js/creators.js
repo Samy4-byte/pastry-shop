7e9d123f76152557980f8ebc1a1909c0de6dccdd
Samy4-byte <samatbayizov@gmail.com>
2023-04-10 12:18:27 +0600

    {

--- js/creators.js
+++ js/creators.js
@@ -1,25 +1,29 @@
+export function createCatalogItem(item) {
+  const itemContainer = document.createElement('div')
+  itemContainer.className = 'catalog__item'
 
- export function createCatalogItem(item) {
-    const itemContainer = document.createElement('div')
-    itemContainer.className = 'catalog__item'
-  
-    const title = document.createElement('h2')
-    title.className = 'catalog__item-title'
-    title.textContent = item.name
-  
-    const image = document.createElement('img')
-    image.setAttribute('src', item.image)
-    image.className = 'catalog__item-image'
-   
-    const price = document.createElement('div')
-    price.className = 'catalog__item-price'
-    price.textContent = `${item.cost} $`
+  const link = document.createElement('a')
+  link.href = `http://127.0.0.1:5500/item.html?id=${item.id}`
 
-    const btn = document.createElement('button')
-    btn.className = 'catalog__item-buy-btn'
-    btn.textContent = 'Buy'
+  const title = document.createElement('h2')
+  title.className = 'catalog__item-title'
+  title.textContent = item.name
 
-    itemContainer.append(title, image, price, btn)
-  
-    return itemContainer
-  }
+  link.append(title)
+
+  const image = document.createElement('img')
+  image.setAttribute('src', item.image)
+  image.className = 'catalog__item-image'
+
+  const price = document.createElement('div')
+  price.className = 'catalog__item-price'
+  price.textContent = `$${item.cost}`
+
+  const buyBtn = document.createElement('button')
+  buyBtn.className = 'catalog__item-buy-btn'
+  buyBtn.textContent = 'Buy'
+
+  itemContainer.append(link, image, price, buyBtn)
+
+  return itemContainer
+}
